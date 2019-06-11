047d57784c4997d00fee3d64cc61388bc3656299
Niels Joubert <njoubert@gmail.com>
2019-06-10 23:04:46 -0700

    slight cleanup

--- thinkpadx60/bashrc.sh
+++ thinkpadx60/bashrc.sh
@@ -18,10 +18,6 @@ shopt -s histappend
 HISTSIZE=4000
 HISTFILESIZE=6000
 
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
-#shopt -s globstar
-
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
